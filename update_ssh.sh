e218cc315d9abb415c1134dac2257ceedcb5f8dc
jdgasser-hmtpro <jdgasser@gmail.com>
2025-10-24 16:04:58 +0200

    Update update_ssh.sh

--- update_ssh.sh
+++ update_ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+mkdir -p ~/.ssh/
 # Ajoute le contenu de /mnt/config/custom_hosts à /etc/hosts
 if [ -f /opt/nagios/etc/ssh/id_rsa ]; then
     cp /opt/nagios/etc/ssh/id_rsa /opt/nagios/.ssh/id_rsa
@@ -13,3 +13,8 @@ if [ -f /opt/nagios/etc/ssh/knwon_hosts ]; then
     cp /opt/nagios/etc/ssh/id_rsa /opt/nagios/.ssh/known_hosts
     echo "copy known_host ok"
 fi
+chmod 700 /opt/nagios/.ssh/
+chmod 600 /opt/nagios/.ssh/id_rsa
+chmod 600 /opt/nagios/.ssh/id_rsa.pub
+chmod 600 /opt/nagios/.ssh/known_hosts
+chown -R nagios:nagios /opt/nagios/.ssh
