e442e846256b28dab225d5c6250e12863525f788
jdgasser-hmtpro <jdgasser@gmail.com>
2026-03-10 18:38:59 +0100

    Fix typo in known_hosts file copy command

--- update_ssh.sh
+++ update_ssh.sh
@@ -9,8 +9,8 @@ if [ -f /opt/nagios/etc/ssh/id_rsa.pub ]; then
     cp /opt/nagios/etc/ssh/id_rsa.pub /opt/nagios/.ssh/id_rsa.pub
     echo "copy id_rsa.pub ok"
 fi
-if [ -f /opt/nagios/etc/ssh/knwon_hosts ]; then
-    cp /opt/nagios/etc/ssh/id_rsa /opt/nagios/.ssh/known_hosts
+if [ -f /opt/nagios/etc/ssh/known_hosts ]; then
+    cp /opt/nagios/etc/ssh/known_hosts /opt/nagios/.ssh/known_hosts
     echo "copy known_host ok"
 fi
 chmod 700 /opt/nagios/.ssh/
@@ -18,5 +18,5 @@ chmod 600 /opt/nagios/.ssh/id_rsa
 chmod 600 /opt/nagios/.ssh/id_rsa.pub
 chmod 600 /opt/nagios/.ssh/known_hosts
 chown -R nagios:nagios /opt/nagios/.ssh
-su - nagios -c 'ssh backup "exit"'
-su - nagios -c 'ssh jd@ns3.hmt-pro.com "exit"'
+#su - nagios -c 'ssh backup "exit"'
+#su - nagios -c 'ssh jd@ns3.hmt-pro.com "exit"'
